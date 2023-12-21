3cc629c6594aae984886041c58f9822f6dddf766
AurelienGuern <aguern96@gmail.com>
2023-12-21 15:32:55 +0100

    dynamisation page home

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use App\Model\MovieModel;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'front_main_home')]
     public function home() : Response
     {
-        $response = new Response('Méthode Home de MainController', Response::HTTP_OK);
+        // On doit récupérer la liste des films
+        $movies = MovieModel::getMovies();
+        // dd($movies);
 
-        return $response;
+        return $this->render('main/home.html.twig', [
+            'movies' => $movies,
+        ]);
     }
 }
