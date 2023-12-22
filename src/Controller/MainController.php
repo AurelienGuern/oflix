ba286ffbedd1e55a3dbce883b03a4017eed1cd75
AurelienGuern <aguern96@gmail.com>
2023-12-22 10:10:20 +0100

    liens dynamisés, tri films par date et nom

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,12 +9,40 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
+    
     #[Route('/', name: 'front_main_home')]
-    public function home() : Response
+    public function home(): Response
     {
         // On doit récupérer la liste des films
         $movies = MovieModel::getMovies();
         // dd($movies);
+        // tri des $movies par ordre de sortie décroissant
+        // REFER : https://www.php.net/manual/fr/function.usort
+        usort($movies, function ($movie1, $movie2) {
+            return $movie2['release_date'] - $movie1['release_date'];
+        });
+        return $this->render('main/home.html.twig', [
+            'movies' => $movies,
+        ]);
+    }
+
+    /**
+     * Affiche les films par ordre alphabétique croissant
+     *
+     * @return Response
+     */
+    #[Route('/movies', name: 'front_main_index')]
+    public function index(): Response
+    {
+        // On doit récupérer la liste des films
+        $movies = MovieModel::getMovies();
+        // dd($movies);
+        // tri des $movies par ordre de alphabétique
+        // REFER : https://www.php.net/manual/fr/function.usort
+        // REFER : https://www.php.net/manual/fr/function.strcasecmp.php
+        usort($movies, function ($movie1, $movie2) {
+            return strcasecmp($movie1['title'], $movie2['title']);
+        });
 
         return $this->render('main/home.html.twig', [
             'movies' => $movies,
@@ -22,15 +50,14 @@ class MainController extends AbstractController
     }
 
     #[Route('/show/{id}', name: 'front_main_show')]
-    public function show($id) : Response
+    public function show($id): Response
     {
-        
+
         $movie = MovieModel::getMovieById($id);
-      
+
 
         return $this->render('main/show.html.twig', [
             'movie' => $movie,
         ]);
     }
 }
-
