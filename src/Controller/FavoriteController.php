ecb585e083f2e9220979a5e0eddbaa5a9357ec39
AurelienGuern <aguern96@gmail.com>
2023-12-22 18:05:07 +0100

    challenge fini, ajout suppression fav, et vider les favoris

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -13,28 +13,32 @@ class FavoriteController extends AbstractController
     #[Route('/favorites', name: 'front_favorites_index')]
     public function index(SessionInterface $session): Response
     {
-        $movies = MovieModel::getFavoriteMovies($session);
-        // dd($movies);
-        // tri des $movies par ordre de sortie décroissant
-        // REFER : https://www.php.net/manual/fr/function.usort
-       
-        return $this->render('favorites/index.html.twig', [
-            'movies' => $movies,
-        ]);
+        // Récupérer tous les films
+        $allMovies = MovieModel::getMovies();
     
-       
+        // Récupérer les films favoris à partir de la session
+        $favoriteMoviesIds = MovieModel::getFavoriteMovies($session);
+    
+        // Filtrer les films pour afficher uniquement les favoris
+        $favoriteMovies = array_filter($allMovies, function ($movie) use ($favoriteMoviesIds) {
+            return in_array($movie['id'], $favoriteMoviesIds);
+        });
+    
+        return $this->render('favorites/index.html.twig', [
+            'movies' => $favoriteMovies,
+        ]);
     }
+    
 
     #[Route('/favorites/add/{id<\d+>}', name: 'front_favorites_add')]
-    public function add(int $id, SessionInterface $session): Response
+public function add(int $id, SessionInterface $session): Response
 {
-    $movie = MovieModel::getMovieById($id);
+    // Ajoute l'ID du film aux favoris dans la session
     $favoriteMovies = $session->get('favorite_movies', []);
-
-    // Vérifie si le film n'est pas déjà dans les favoris
-    if (!in_array($movie, $favoriteMovies)) {
-        // Ajoute le film à la liste des favoris
-        $favoriteMovies[] = $movie;
+    
+    // Vérifie si le film est déjà dans les favoris
+    if (!in_array($id, $favoriteMovies)) {
+        $favoriteMovies[] = $id;
         $session->set('favorite_movies', $favoriteMovies);
 
         $this->addFlash(
@@ -52,29 +56,46 @@ class FavoriteController extends AbstractController
 }
 
 
-    
 
     #[Route('/favorites/remove/{id<\d+>}', name: 'front_favorites_remove')]
     public function remove(int $id, SessionInterface $session): Response
     {
-        $movie = MovieModel::getMovieById($id);
-        $movie = $session->set('favorite', 'no');
+        $favoriteMovies = $session->get('favorite_movies', []);
+    
+        // Recherche l'index du film dans le tableau des favoris
+        $index = array_search($id, $favoriteMovies);
+    
+        if ($index !== false) {
+            // Si le film est trouvé dans les favoris, supprimez-le du tableau
+            unset($favoriteMovies[$index]);
+            $session->set('favorite_movies', array_values($favoriteMovies));
+    
+            $this->addFlash(
+                'alert',
+                'Le film a été supprimé de vos favoris !'
+            );
+        } else {
+            $this->addFlash(
+                'info',
+                'Le film ne faisait pas partie de vos favoris.'
+            );
+        }
+    
+        return $this->redirectToRoute('front_favorites_index');
+    }
 
+    #[Route('/favorites/remove/all', name: 'front_favorites_empty')]
+    public function empty(SessionInterface $session): Response
+    {
+        // Supprime complètement la clé des favoris de la session
+        $session->remove('favorite_movies');
+    
         $this->addFlash(
             'alert',
-            'a été tej de vos favoris !'
+            'Tous les favoris ont été supprimés !'
         );
-        return $this->redirectToRoute(('front_favorites_index'));
+    
+        return $this->redirectToRoute('front_main_home');
     }
+    
 }
-
-
-
-
-
-
-// $this->addFlash('danger', 
-// 'a été supprimé de vos favoris !');
-
-// return $this->redirectToRoute(('front_favorites_index'));
-// }
