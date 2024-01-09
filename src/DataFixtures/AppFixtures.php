b84c644d65e0ae2ee0862f100dfac7d988953a27
AurelienGuern <aguern96@gmail.com>
2024-01-09 15:34:28 +0100

    fixture avec Faker et cinema Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use Faker\Generator;
+
 use App\Entity\Genre;
 use App\Entity\Movie;
 use App\Entity\Person;
@@ -11,45 +14,52 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+    
     public function load(ObjectManager $manager): void
     {
+        
 $movie = null;
 $person = null;
 
         // création de 20 acteurs
         for ($i = 1; $i < 21; $i++) {
             $person = new Person();
-            $person->setFirstname('Prénom' . $i);
-            $person->setLastname('Nom' . $i);
+            $person->setFirstname($this->faker->actor());
+            $person->setLastname(' ');
             $manager->persist($person);
         }
 
         for ($i = 1; $i < 21; $i++) {
             $genre = new Genre();
-            $genre->setName('nom de genre ' . $i);
+            $genre->setName($this->faker->movieGenre());
             $manager->persist($genre);
         }
 
         // création de 40 movie
         for ($i = 1; $i < 41; $i++) {
-            $post = new Movie();
-            $post->setTitle('Titre ' . $i);
-            $post->setType(rand(0, 1) ? 'Movie' : 'Série');
-            $post->setSummary('Résuméééééééééé ' . $i);
-            $post->setSynopsis('Synops ' . $i);
-            $post->setPoster('https://1.bp.blogspot.com/-iCnFX7eWVjs/XR9NQutHXcI/AAAAAAAAJ9k/ISWH3UXgJF8QJdsV6P9wh3agzOwOF_aYgCLcBGAs/s1600/cat-1285634_1920.png');
-            $post->setRating(rand(1, 50) / 10 + 1);
-            $post->setReleaseDate(new \DateTimeImmutable());
-            $post->setDuration(rand(1, 200));
-
-            $manager->persist($post);
+            $movie = new Movie();
+            $movie->setTitle($this->faker->movie());
+            $movie->setType(rand(0, 1) ? 'Movie' : 'Série');
+            $movie->setSummary($this->faker->sentence(4));
+            $movie->setSynopsis($this->faker->sentence(2));
+            $movie->setPoster('https://1.bp.blogspot.com/-iCnFX7eWVjs/XR9NQutHXcI/AAAAAAAAJ9k/ISWH3UXgJF8QJdsV6P9wh3agzOwOF_aYgCLcBGAs/s1600/cat-1285634_1920.png');
+            $movie->setRating(rand(1, 50) / 10 + 1);
+            $movie->setReleaseDate(new \DateTimeImmutable());
+            $movie->setDuration(rand(40, 200));
+            $manager->persist($movie);
         }
 
         for ($i = 1; $i < 25; $i++) {
             $post = new Casting();
             $post->setMovie($movie);
             $post->setPerson($person);
-            $post->setRole('je possède un rôle');
+            $post->setRole($this->faker->word());
             $post->setCreditOrder(rand(1, 10));
            
             $manager->persist($post);
