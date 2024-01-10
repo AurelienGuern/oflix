208ead6e51c0aea991b5fccbc24fa7dcc3d5efbc
AurelienGuern <aguern96@gmail.com>
2024-01-10 11:39:09 +0100

    faker avec image et movie provider

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,80 +2,110 @@
 
 namespace App\DataFixtures;
 
-use Faker\Factory;
-use Faker\Generator;
 
+use Faker\Factory;
 use App\Entity\Genre;
 use App\Entity\Movie;
 use App\Entity\Person;
+use App\Entity\Season;
 use App\Entity\Casting;
+use Bluemmb\Faker\PicsumPhotosProvider;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\DataFixtures\Provider\OflixProvider;
 
 class AppFixtures extends Fixture
 {
-    private Generator $faker;
+    private $genres  = [];
+    private $persons = [];
 
-    public function __construct()
-    {
-        $this->faker = Factory::create('fr_FR');
-    }
-    
     public function load(ObjectManager $manager): void
     {
-        
-$movie = null;
-$person = null;
+        // create a French faker
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Xylis\FakerCinema\Provider\Movie($faker));
+        $faker->addProvider(new \Xylis\FakerCinema\Provider\TvShow($faker));
+        $faker->addProvider(new \Xylis\FakerCinema\Provider\Person($faker));
+        $faker->addProvider(new \Xylis\FakerCinema\Provider\Character($faker));
+        $faker->addProvider(new PicsumPhotosProvider($faker));
+        $faker->addProvider(new OflixProvider);
 
-        // création de 20 acteurs
-        for ($i = 1; $i < 21; $i++) {
-            $person = new Person();
-            $person->setFirstname($this->faker->name());
-            $person->setLastname($this->faker->name());
-            $manager->persist($person);
-        }
-
-        for ($i = 1; $i < 21; $i++) {
-            $genre = new Genre();
-            $genre->setName($this->faker->word());
+        // création de 20 genres
+        for ($i = 0; $i < 20; $i++) {
+            $genre = new Genre;
+            $genre->setName($faker->unique()->movieGenre());
             $manager->persist($genre);
+            $this->genres[] = $genre;
         }
 
-        // création de 40 movie
-        for ($i = 1; $i < 41; $i++) {
-            $movie = new Movie();
-            $movie->setTitle($this->faker->sentence());
-            $movie->setType(rand(0, 1) ? 'Movie' : 'Série');
-            $movie->setSummary($this->faker->sentence(4));
-            $movie->setSynopsis($this->faker->sentence(2));
-            $movie->setPoster('https://1.bp.blogspot.com/-iCnFX7eWVjs/XR9NQutHXcI/AAAAAAAAJ9k/ISWH3UXgJF8QJdsV6P9wh3agzOwOF_aYgCLcBGAs/s1600/cat-1285634_1920.png');
-            $movie->setRating(rand(1, 50) / 10 + 1);
-            $movie->setReleaseDate(new \DateTimeImmutable());
-            $movie->setDuration(rand(40, 200));
+        // on crée des personnes
+        for ($i = 0; $i < 190; $i++) {
+            // on récupère un acteur et on éclate en deux
+            $actor = explode(' ', $faker->unique()->actor(), 2);
+            $person = new Person;
+            $person->setFirstname($actor[0]);
+            $person->setLastname($actor[1]);
+            $manager->persist($person);
+            $this->persons[] = $person;
+        }
+
+        // Movie : Film
+
+        for ($i = 0; $i < 50; $i++) {
+            $movie = new Movie;
+            // si un movie est une série alors il y a des saison
+            if ($faker->boolean()) 
+            {
+                // c'est un film
+                $movie->setTitle($faker->unique()->movie());
+                $movie->setType('Film');
+                $movie->setDuration(random_int(80, 330));
+            }
+            else
+            {
+                // c'est une série
+                $movie->setTitle($faker->unique()->tvShow());
+                $movie->setType('Série');
+                $movie->setDuration(random_int(25, 60));
+                // il y a aussi des saisons
+                for ($j = 1; $j < random_int(2, 12); $j++) {
+                    $season = new Season;
+                    $season->setNumber($j);
+                    $season->setEpisodeNumber(random_int(6, 12));
+                    $season->setMovie($movie);
+                    $manager->persist($season);
+                }
+            }
+
+            $movie->setReleaseDate(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween()));
+            $movie->setSummary($faker->realText(60));
+            $movie->setSynopsis($faker->sentence(4));
+            $movie->setPoster($faker->imageUrl(200, 300, true));
+            $movie->setRating(rand(1, 50) /50);
+
+            // on associe entre 0 et 4 genres à un movie
+            for ($j=0; $j < random_int(0, 5); $j++) { 
+                $movie->addGenre($faker->unique()->randomElement($this->genres));
+            }
+
+            // on associe les personnes et les films avec Casting
+            // jusqu'à 10 acteurs par movie
+            // avec le creditOrder dans le désordre
+            $orders = range(0, random_int(0, 10));
+            shuffle($orders);
+            if (count($orders) != 1) {
+                foreach ($orders as $order) {
+                    $casting = new Casting;
+                    $casting->setMovie($movie);
+                    $casting->setPerson($this->persons[random_int(0, count($this->persons) - 1)]);
+                    $casting->setCreditOrder($order+1);
+                    $casting->setRole($faker->character());
+                    $manager->persist($casting);
+                }
+            }
+
             $manager->persist($movie);
         }
-
-        for ($i = 1; $i < 25; $i++) {
-            $post = new Casting();
-            $post->setMovie($movie);
-            $post->setPerson($person);
-            $post->setRole($this->faker->word());
-            $post->setCreditOrder(rand(1, 10));
-           
-            $manager->persist($post);
-        }
-
-        for ($i = 1; $i < 25; $i++) {
-            $post = new Casting();
-            $post->setMovie($movie);
-            $post->setPerson($person);
-            $post->setRole($this->faker->word());
-            $post->setCreditOrder(rand(1, 10));
-           
-            $manager->persist($post);
-        }
-
-    
         $manager->flush();
     }
 }
