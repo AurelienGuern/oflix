184ed98436f1acf3ed30040de2288d3767c70125
AurelienGuern <aguern96@gmail.com>
2024-01-11 11:47:23 +0100

    traitement du formulaire

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -5,19 +5,29 @@ namespace App\Controller;
 use App\Entity\Movie;
 use App\Entity\Review;
 use App\Form\ReviewType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReviewController extends AbstractController
 {
     #[Route('/review/{id<\d+>}', name: 'front_review_new')]
-    public function new(Movie $movie): Response
+    public function new(Movie $movie, Request $request, EntityManagerInterface $entityManager): Response
     {
         $review = new Review;
         $form = $this->createForm(ReviewType::class, $review);
 
-        // TODO Gestion du formulaire
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $review->setMovie($movie);
+
+            $entityManager->persist($review);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('front_main_show', ['id' => $movie->getId()]);
+        }
 
         return $this->render('review/new.html.twig', [
             'movie' => $movie,
