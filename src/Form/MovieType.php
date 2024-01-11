a53d9e33ad20ffdc6205df2812b994a3c28e57d7
AurelienGuern <aguern96@gmail.com>
2024-01-11 14:27:17 +0100

    CRUD back

---
+++ src/Form/MovieType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Genre;
+use App\Entity\Movie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('releaseDate')
+            ->add('duration')
+            ->add('title')
+            ->add('poster')
+            ->add('rating')
+            ->add('type')
+            ->add('Summary')
+            ->add('Synopsis')
+            ->add('genres', EntityType::class, [
+                'class' => Genre::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
