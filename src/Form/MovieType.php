d591955c3d342b6642c125f89b609a9115da5e8f
AurelienGuern <aguern96@gmail.com>
2024-01-12 10:33:16 +0100

    maj formulaire

--- src/Form/MovieType.php
+++ src/Form/MovieType.php
@@ -7,12 +7,13 @@ use App\Entity\Movie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-
-
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class MovieType extends AbstractType
 {
@@ -20,32 +21,48 @@ class MovieType extends AbstractType
     {
         $builder
             ->add('title', TextType::class, [
-                'label'     => 'Titre du film ou de la série',
+                'label'         => 'Titre du film ou de la série',
+                'empty_data'    => '',
             ])
-            ->add('releaseDate',DateType::class, [
+            ->add('releaseDate', DateType::class, [
+                'label'             => 'Date de sortie',
                 'widget'    => 'single_text',
                 'input'     => 'datetime_immutable',
                 'empty_data' => (new \DateTimeImmutable())->format('d/m/Y'),
+                'invalid_message'   => 'Veuillez entrer une date de sortie valide',
             ])
-            ->add('duration')
-            ->add('summary')
-            ->add('synopsis')
-            ->add('poster')
+            ->add('duration', IntegerType::class, [
+                'label'     => 'Durée du film ou de la série',
+                'empty_data' => '0',
+            ])
+            ->add('summary', TextareaType::class, [
+                'label'     => 'Résumé',
+                'empty_data' => '',
+            ])
+            ->add('synopsis', null, [
+                'label'     => 'Synopsis',
+                'empty_data' => '',
+            ])
+            ->add('poster', UrlType::class, [
+                'label'     => 'Affiche',
+                'empty_data' => '',
+            ])
             ->add('type', ChoiceType::class, [
-                'choices'  => [
-                    'Film'         => 'Film',
-                    'Série'          => 'Série',
-                  
+                'label'     => 'Type',
+                'multiple'  => false,
+                'expanded'  => true,
+                'choices'   => [
+                    'Film'  => 'Film',
+                    'Série' => 'Série',
                 ],
-                'label' => "Type ",
+                'empty_data' => '',
             ])
             ->add('genres', EntityType::class, [
                 'class'         => Genre::class,
                 'choice_label'  => 'name',
                 'multiple'      => true,
                 'expanded'      => true,
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
