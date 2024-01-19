a0ff3359d9e6e59dfa7d2a060a74b998d6e70433
AurelienGuern <aguern96@gmail.com>
2024-01-19 13:52:27 +0100

    API omdb

---
+++ src/Service/OmdbApi.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class OmdbApi
+{
+    public function __construct(
+        private HttpClientInterface $client,
+    ) {
+    }
+    public function fetchPoster(string $title)
+    {
+        // appeller l'api de omdb
+        $response = $this->client->request(
+            'GET',
+            'http://www.omdbapi.com/', [
+                // these values are automatically encoded before including them in the URL
+                'query' => [
+                    'apikey' => '5ae01ce1',
+                    't' => $title,
+                ],
+            ]);
+
+            $statusCode = $response->getStatusCode();
+            // $statusCode = 200
+            if ($statusCode != 200) {
+                return null;
+            }
+            // récupérer le JSON sous forme de tableau
+            $content = $response->toArray();
+            // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+            // vérifier si cette entrée existe
+            if (!isset($content['Poster'])) {
+                return null;
+            }
+            
+            // récupérer l'entrée Poster du json
+            return $content['Poster'];
+        }
+    }
