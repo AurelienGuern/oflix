bdbcdaf9f00a5b5340bd221aa3dcc6edac878ae9
AurelienGuern <aguern96@gmail.com>
2024-01-19 14:35:11 +0100

    movie psoter comand ok

--- src/Command/MoviesPosterCommand.php
+++ src/Command/MoviesPosterCommand.php
@@ -3,8 +3,7 @@
 namespace App\Command;
 
 use App\Repository\MovieRepository;
-use App\Service\MySlugger;
-use Doctrine\ORM\EntityManager;
+use App\Service\OmdbApi;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -15,14 +14,14 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
-    name: 'movies:slugify',
-    description: 'Création des slugs pour les titres en base',
+    name: 'movies:poster',
+    description: 'Ajouter l\'affiche du film en fonction de son titre',
 )]
-class MoviesSlugifyCommand extends Command
+class MoviesPosterCommand extends Command
 {
     public function __construct(
         private MovieRepository $movieRepository,
-        private MySlugger $slugger,
+        private OmdbApi $omdbApi,
         private EntityManagerInterface $entityManager
     )
     {
@@ -33,37 +32,33 @@ class MoviesSlugifyCommand extends Command
     {
         $this
             ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
-
-        if ($input->getOption('option1')) {
-            // ...
-        }
-        // dans cette partie, on rentre la logique de notre commande
-
-        // on veut "slugifier" les titres des films en base de donnée
-        // Récupérer ces titres
+        // on récupère tous les films
         $movies = $this->movieRepository->findAll();
-        // parcourir les movies
+        // pour chaque film on récupère le poster s'il existe
         foreach ($movies as $movie) {
-            // leur appliquer le slugify
-            $movie->setSlug($this->slugger->slugify($movie->getTitle()));
-            // on persiste le movie
-            $this->entityManager->persist($movie);
+            $poster = $this->omdbApi->fetchPoster($movie->getTitle());
+            // si le poster existe on modifie $movie, sinon on laisse tel quel
+            if ($poster != null) {
+                $movie->setPoster($poster);
+                $this->entityManager->persist($movie);
+                $io->success($movie->getTitle() . ' : affiche mise à jour.');
+            } else {
+                $io->warning($movie->getTitle() . ' n\'a pas pu être mis à jour.');
+            }
+            
         }
-        // les sauvegarder
+        // on sauvegarde en base de donnée
         $this->entityManager->flush();
 
-        $io->success('La mise à jour des slugs est terminée');
+        $io->success('La mise à jour des affiches est terminée.');
 
         return Command::SUCCESS;
     }
