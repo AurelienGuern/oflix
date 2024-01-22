df3f7c55aeb36f9db8ab403a630e3b7def8c4b5c
AurelienGuern <aguern96@gmail.com>
2024-01-22 16:34:23 +0100

    str replace

--- src/EventSubscriber/MaintenanceSubscriber.php
+++ src/EventSubscriber/MaintenanceSubscriber.php
@@ -4,19 +4,29 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class MaintenanceSubscriber implements EventSubscriberInterface
 {
-    public function onKernelResponse(ResponseEvent $event): void
-    {
-        // ...
-    }
+   
 
     public static function getSubscribedEvents(): array
     {
         return [
-            KernelEvents::RESPONSE => 'onKernelResponse',
+            KernelEvents::RESPONSE => 'onKernelController',
         ];
     }
+
+    public function onKernelController(ResponseEvent $event): void
+    {
+        
+     $response = $event->getResponse();
+     $content = $response->getContent();
+     $content = str_replace('<body>', '<body><div class="alert alert-danger">Maintenance prévue mardi 10 janvier à 17h00</div>', $content);
+ 
+     $response->setContent($content);
+
+ }
+ 
 }
