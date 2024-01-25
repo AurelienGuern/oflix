e3b18bb2c52c70778ac2e3bbc58afe5913f8d559
AurelienGuern <aguern96@gmail.com>
2024-01-25 12:00:24 +0100

    MAJ tests

--- tests/Service/MySluggerTest.php
+++ tests/Service/MySluggerTest.php
@@ -11,15 +11,20 @@ class MySluggerTest extends TestCase
 {
     public function testSlugify(): void
     {
+        // on crée un tableau de tests avec l'élément envoyé et l'élément attendu et le isLower
         $tests = [
-            ['Jo Le Taxi', 'Jo-Le-Taxi'],
-            ['Bonjour ça va 22 ?', 'Bonjour-ca-va-22']
+            ['Jo le Taxi', 'Jo-le-Taxi', false],
+            ['Bonjour ça va bien', 'Bonjour-ca-va-bien', false],
+            ['avec nombre 42 ?', 'avec-nombre-42', false],
+            ['Jo le Taxi', 'jo-le-taxi', true],
+            ['Bonjour ça va bien', 'bonjour-ca-va-bien', true],
+            ['avec nombre 42 ?', 'avec-nombre-42', true],
         ];
 
-
-        $slugger = new MySlugger(new AsciiSlugger(), false);
         foreach ($tests as $test) {
-            $this->assertEquals($test[1], $slugger->slugify($test[0]));
+            $slugger = new MySlugger(new AsciiSlugger(), $test[2]);
+            $this->assertEquals($test[1],$slugger->slugify($test[0]));
         }
     }
+    
 }
