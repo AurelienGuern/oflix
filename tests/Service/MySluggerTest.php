979767f487af1ebadd908f88f5c6edd5655eba05
AurelienGuern <aguern96@gmail.com>
2024-01-25 11:56:29 +0100

    tests tableau

--- tests/Service/MySluggerTest.php
+++ tests/Service/MySluggerTest.php
@@ -11,9 +11,15 @@ class MySluggerTest extends TestCase
 {
     public function testSlugify(): void
     {
+        $tests = [
+            ['Jo Le Taxi', 'Jo-Le-Taxi'],
+            ['Bonjour ça va 22 ?', 'Bonjour-ca-va-22']
+        ];
 
-        
+
         $slugger = new MySlugger(new AsciiSlugger(), false);
-        $this->assertEquals('Jo-Le-Taxi', $slugger->slugify('Jo Le Taxi'));
+        foreach ($tests as $test) {
+            $this->assertEquals($test[1], $slugger->slugify($test[0]));
+        }
     }
 }
