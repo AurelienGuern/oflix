1a9a3ffa8e2a4a1ed2d947086be715a203dfa07f
AurelienGuern <aguern96@gmail.com>
2024-01-25 14:48:45 +0100

    ACL test

--- tests/Web/AclTest.php
+++ tests/Web/AclTest.php
@@ -2,12 +2,58 @@
 
 namespace App\Tests\Web;
 
-use PHPUnit\Framework\TestCase;
+use PhpParser\Node\Expr\Cast\Array_;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class AclTest extends TestCase
+class AclTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testHome(): void
     {
-        $this->assertTrue(true);
+        // on crée un instance d'un navigateur
+        $client = static::createClient();
+        // on envoie une requête
+        $crawler = $client->request('GET', '/');
+
+        // on teste si la route répond bien
+        $this->assertResponseIsSuccessful();
+        // on teste si le h1 de la page contient ce que le client a demandé
+        $this->assertSelectorTextContains('h1', 'Films, séries TV et popcorn en illimité.');
+    }
+
+    /**
+     * @dataProvider urlNotConnectedProvider
+     */
+    public function testNotConnected($url, $codeRetour): void
+    {
+        // on crée un instance d'un navigateur
+        $client = static::createClient();
+        // on envoie une requête
+        $crawler = $client->request('GET', $url);
+
+        $this->assertResponseStatusCodeSame($codeRetour);
+    }
+
+    // Utilisation d'un dataprovider
+    // REFER : https://docs.phpunit.de/en/10.5/writing-tests-for-phpunit.html#data-providers
+    public static function urlNotConnectedProvider(): array
+    {
+        return [
+            ['/', 200],
+            ['/movies', 200],
+            ['/show/Chinatown', 200],
+            ['/review/42', 302]
+        ];
+    }
+
+        // Utilisation d'un dataprovider
+    // REFER : https://docs.phpunit.de/en/10.5/writing-tests-for-phpunit.html#data-providers
+    public static function urlConnectedProvider(): array
+    {
+        return [
+            ['/', 200],
+            ['/movies', 200],
+            ['/show/Chinatown', 200],
+            ['/review/42', 200]
+        ];
     }
 }
