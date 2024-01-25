9000def49569ea1b9241a1515b02cf54d34be5c2
AurelienGuern <aguern96@gmail.com>
2024-01-25 13:44:48 +0100

    tests améliorés

--- tests/Service/MySluggerTest.php
+++ tests/Service/MySluggerTest.php
@@ -9,10 +9,20 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class MySluggerTest extends TestCase
 {
-    public function testSlugify(): void
+    /**
+     * @dataProvider slugProvider
+     */
+    public function testSlugify($title, $expected, $isLower): void
     {
-        // on crée un tableau de tests avec l'élément envoyé et l'élément attendu et le isLower
-        $tests = [
+            $slugger = new MySlugger(new AsciiSlugger(), $isLower);
+            $this->assertEquals($expected,$slugger->slugify($title));
+    }
+
+    // REFER : https://docs.phpunit.de/en/9.6/writing-tests-for-phpunit.html#data-providers
+    public static function slugProvider(): Array
+    {
+        // on retourne un tableau de tests avec l'élément envoyé et l'élément attendu et le isLower
+        return [
             ['Jo le Taxi', 'Jo-le-Taxi', false],
             ['Bonjour ça va bien', 'Bonjour-ca-va-bien', false],
             ['avec nombre 42 ?', 'avec-nombre-42', false],
@@ -20,11 +30,5 @@ class MySluggerTest extends TestCase
             ['Bonjour ça va bien', 'bonjour-ca-va-bien', true],
             ['avec nombre 42 ?', 'avec-nombre-42', true],
         ];
-
-        foreach ($tests as $test) {
-            $slugger = new MySlugger(new AsciiSlugger(), $test[2]);
-            $this->assertEquals($test[1],$slugger->slugify($test[0]));
-        }
     }
-    
 }
