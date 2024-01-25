cfcc1a161da6888215fe8d20b71f4b80c90aa67a
AurelienGuern <aguern96@gmail.com>
2024-01-25 14:20:03 +0100

    Web\AclTest

---
+++ tests/Web/AclTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Web;
+
+use PHPUnit\Framework\TestCase;
+
+class AclTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
